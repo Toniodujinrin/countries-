60db03e8cc7347c7a800b23addfba4a357fbb061
Toniodujinrin <todujinrin@gmail.com>
2022-08-14 17:56:49 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Home from './HomePage';
+class App extends Component {
+  state = {  } 
+  
+  
+  render() { 
+    return (
+   <Home/>
+    );
+  }
+}
+ 
+export default App;
